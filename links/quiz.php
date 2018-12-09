5d2be8de77a49361b5c91068a0b624e98026b5cb
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-09 21:51:08 +0100

    wroteQuizContent

--- links/quiz.php
+++ links/quiz.php
@@ -1,7 +1,13 @@
 <?php
     return 
-    "
-    <p>Welcome to quiz</p>
+    "<form method='post' action='index.php?page=quiz'>
+        <p>Is it har or fun to learn PHP?</p>
+        <select name='answer'>
+            <option value='Yes'>Yes</option>
+            <option value='No'>No</option>
+            <input type='submit' name='quize-submitted' value='post'>
+        </select>
+    </form>
 
     "
 ?>
