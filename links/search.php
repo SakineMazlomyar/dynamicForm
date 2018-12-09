59a4a4ec11e074be97114b3ac0029a618cb227e3
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-09 20:44:54 +0100

    addedSearchForm

---
+++ links/search.php
@@ -0,0 +1,10 @@
+<?php
+    return 
+    "
+    <label>Name</label><br>
+    <form method='get' action='htto://www.bing.com/search'>
+        <input type='text' name='q'><br>
+        <input type='submit' value='search on bing'>
+    </form>
+    "
+?>
