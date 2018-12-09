ab2e3b3b4d222dd3e26c7b80749aacf652ec1fe5
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-09 21:52:01 +0100

    addedPostMethod

--- index.php
+++ index.php
@@ -2,14 +2,28 @@
     include_once("class/classes.php");
     $newInstance = new Info();
     $newInstance->content = include_once("links/navigation.php");
-    $valueOfPage = isset($_GET['page']);
-    if($valueOfPage){
-        $fileShouldLoad = $_GET['page'];
+    $newInstance->contentTwo = include_once("links/quiz.php");
+    $valueOfLinkClikeckTwo = isset($_POST['quize-submitted']);
+    if($valueOfLinkClikeckTwo){
+        $answer = $_POST['answer'];
+        $outPut = showQuizAnswer($answer);
     }else{
+        $outPut = include_once("links/quiz.php");
+    }
+    echo($outPut);
+    function showQuizAnswer($answer){
+        return $answer;
+    };
+
+    $valueOfLinkClicked = isset($_GET['page']);
+    if($valueOfLinkClicked){
+        $fileShouldLoad = $_GET['page'];
+        $newInstance->content.= include_once("links/$fileShouldLoad.php");
+    }/* else{
         $fileShouldLoad = 'search';
     };
-    $newInstance->content.= include_once("links/$fileShouldLoad.php");
-    
+     */
+
 
     $page = include_once("templates/page.php");
     echo("$page")
