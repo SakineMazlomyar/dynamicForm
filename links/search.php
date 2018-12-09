ea16d92f94fe38fedadd80f0a373aa3689fb6c18
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-09 21:50:53 +0100

    checkedLink

--- links/search.php
+++ links/search.php
@@ -2,7 +2,7 @@
     return 
     "
     <label>Name</label><br>
-    <form method='get' action='htto://www.bing.com/search'>
+    <form method='get' action='http://www.bing.com/search'>
         <input type='text' name='q'><br>
         <input type='submit' value='search on bing'>
     </form>
