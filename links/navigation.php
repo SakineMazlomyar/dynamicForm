3ad8f51a6ec60e11d505dbb303109c4445965033
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-09 20:45:49 +0100

    addedNavigation

---
+++ links/navigation.php
@@ -0,0 +1,9 @@
+<?php
+    return 
+    "
+    <nav>
+        <li><a href='index.php?page=search'>Search by bing</a></li>
+        <li><a href='index.php?page=quiz'>Dynamic quiz</a></li>
+    </nav>
+    "
+?>
