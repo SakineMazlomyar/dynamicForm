b518f0446aa9adf670ddfcf6ecc5c8cda0fc6a0f
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-12 15:21:56 +0100

    addedQuizLink

--- links/navigation.php
+++ links/navigation.php
@@ -1,8 +1,8 @@
 <?php
     return 
-    "
-    <nav>
-        <li><a href='index.php?page=search'>Search by bing</a></li>
+    "<nav>
+        <li><a href='index.php?page=search'>Search on bingo</a></li>
+        <li><a href='index.php?page=quiz'>Dynamic quiz</a></li>
     </nav>
     "
 ?>
