20562257de76330a3fe919e46d8567c0e16967b2
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-12 15:21:45 +0100

    addedComment

--- links/search.php
+++ links/search.php
@@ -1,8 +1,8 @@
 <?php
     return 
     "
-    <label>Name</label><br>
     <form method='get' action='http://www.bing.com/search'>
+        <!--a name attribute declares the name of URL varibale-->
         <input type='text' name='q'><br>
         <input type='submit' value='search on bing'>
     </form>
