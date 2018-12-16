312e96a7c4633aff89f4dbae97cf145b0b51bcd7
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-16 19:31:03 +0100

    addedWelcomeText

--- links/navigation.php
+++ links/navigation.php
@@ -1,6 +1,7 @@
 <?php
     return 
     "<nav>
+        <h1>Welcome to This page<h1>
         <li><a href='index.php?page=search'>Search on bingo</a></li>
         <li><a href='index.php?page=quiz'>Dynamic quiz</a></li>
     </nav>
